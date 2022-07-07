d251562c629f0cff6bb9503a3e8f801a31cdce26
gaokefei <260480378@qq.com>
2022-07-07 16:17:05 +0800

    feat: 一个小时刷新一次

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,10 +3,18 @@ import One from './one.vue'
 import Two from './two.vue'
 import Three from './three.vue'
 import Four from './four.vue'
+
+let key = $ref(0)
+const interval = setInterval(() => {
+  key++
+}, 60 * 60 * 1000)
+onUnmounted(() => {
+  clearInterval(interval)
+})
 </script>
 
 <template>
-  <main grid="~ cols-2 rows-2 text-2xl" h-screen>
+  <main :key="key" grid="~ cols-2 rows-2 text-2xl" h-screen>
     <One />
     <Two />
     <Three />
